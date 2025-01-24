3063f79e777c6f9a7ea7291e65c0788ae1d12075
Kyle Rabbitt <119811866+kyler26@users.noreply.github.com>
2025-01-23 23:17:28 -0500

    remove spi defines non sd

--- MCU_Code/MCU_Code.c
+++ MCU_Code/MCU_Code.c
@@ -7,23 +7,12 @@
 #include "sensors.h"
 #include "sdCard.h"
 
-// SPI Defines
-// We are going to use SPI 0, and allocate it to the following GPIO pins
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define SPI_PORT spi0
-#define PIN_MISO 16
-#define PIN_CS   17
-#define PIN_SCK  18
-#define PIN_MOSI 19
-
 
 
 int64_t alarm_callback(alarm_id_t id, void *user_data) {
     // Put your timeout handler code in here
     return 0;
 }
-
-
 
 
 int main()
