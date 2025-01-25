72166d798fbe5942a4d6a82dd26ecfb7bc1dde54
Frank <fparsons143@gmail.com>
2025-01-25 09:27:39 -0500

    added incremental conductance

--- MCU_Code/algorithms.c
+++ MCU_Code/algorithms.c
@@ -4,6 +4,7 @@ float duty_cycle = 0.5;
 float duty_min = 0;
 float duty_max = 0.95;
 float P_O_step = 0.03;
+float I_C_step = 0.00005;
 
 float duty;
 float voltage;
@@ -37,6 +38,50 @@ float perturb_and_observe(){
         }
         else {
             duty = prevDuty - P_O_step;
+        }
+    }
+
+    if (duty >= duty_max || duty <= duty_min) {
+        duty = prevDuty;
+    }
+
+    prevDuty = duty;
+    prevVoltage = voltage;
+    prevPower = power;
+
+    return duty;
+}
+
+float incremental_conductance(){
+
+    power = voltage * current;
+    float deltaV = voltage - prevVoltage;
+    float deltaI = current - prevCurrent;
+
+    if (deltaV ==  0) {
+        if (deltaI == 0){
+            duty = prevDuty; 
+        }
+        else {
+            if(deltaI > 0){
+                duty = prevDuty - I_C_step;
+            }
+            else {
+                duty = prevDuty + I_C_step;
+            } 
+        }
+    }
+    else {
+        if(deltaI/deltaV == -(current/voltage)) {
+            duty = prevDuty;
+        }
+        else {
+            if(deltaI/deltaV > -(current/voltage)) {
+                duty = prevDuty - I_C_step;
+            }
+            else {
+                duty = prevDuty + I_C_step; 
+            }
         }
     }
 
