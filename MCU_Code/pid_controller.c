b31f0e4577fd75f0841f2ec2e8dd237a10c19327
Frank <fparsons143@gmail.com>
2025-01-31 21:51:52 -0500

    directly converted pid c++ file to c

--- MCU_Code/pid_controller.c
+++ MCU_Code/pid_controller.c
@@ -1,38 +1,115 @@
 /*
-   Arduino PID Library - Version 1.2.1
-   by Brett Beauregard <br3ttb@gmail.com> brettbeauregard.com
-   Ported to the Raspberry Pi Pico by Samyar Sadat Akhavi.
+  Arduino PID Library - Version 1.2.1
+  by Brett Beauregard <br3ttb@gmail.com> brettbeauregard.com
+  Ported to the Raspberry Pi Pico by Samyar Sadat Akhavi.
 
-   Original library: https://github.com/br3ttb/Arduino-PID-Library
-   Forked version for Pico: https://github.com/samyarsadat/Pico-PID-Library
+  Original library: https://github.com/br3ttb/Arduino-PID-Library
+  Forked version for Pico: https://github.com/samyarsadat/Pico-PID-Library
 
-   This Library is licensed under the MIT License.
+  This Library is licensed under the MIT License.
 */
 
 #include "pico/stdlib.h"
-#include "pid_controller.h"
+#include <stdbool.h>
+
+// Constants used in some of the functions below
+#define AUTOMATIC  1
+#define MANUAL     0
+#define DIRECT     0
+#define REVERSE    1
+#define P_ON_M     0
+#define P_ON_E     1
+
+typedef struct {
+    // Pointers to Input, Output, Setpoint
+    float *myInput;
+    float *myOutput;
+    float *mySetpoint;
+    
+    // PID tuning parameters
+    float kp;
+    float ki;
+    float kd;
+    
+    // Display tuning parameters
+    float dispKp;
+    float dispKi;
+    float dispKd;
+    
+    // Controller direction
+    int controllerDirection;
+    
+    // POn enum
+    int pOn;
+    bool pOnE;
+    
+    // Output limits
+    float outMin;
+    float outMax;
+    
+    // Sample time
+    unsigned long SampleTime;
+    
+    // Derived PID variables
+    float outputSum;
+    float lastInput;
+    
+    // Mode
+    bool inAuto;
+    
+    // Timing
+    unsigned long lastTime;
+} PID;
+
+// Function prototypes
+void PID_Init(PID *pid, float *Input, float *Output, float *Setpoint, float Kp, float Ki, float Kd, int POn, int ControllerDirection);
+void PID_Init_default(PID *pid, float *Input, float *Output, float *Setpoint, float Kp, float Ki, float Kd, int ControllerDirection);
+bool PID_Compute(PID *pid);
+void PID_SetTunings(PID *pid, float Kp, float Ki, float Kd, int POn);
+void PID_SetTunings_basic(PID *pid, float Kp, float Ki, float Kd);
+void PID_SetSampleTime(PID *pid, int NewSampleTime);
+void PID_SetOutputLimits(PID *pid, float Min, float Max);
+void PID_SetMode(PID *pid, int Mode);
+void PID_Initialize(PID *pid);
+void PID_SetControllerDirection(PID *pid, int Direction);
+float PID_GetKp(PID *pid);
+float PID_GetKi(PID *pid);
+float PID_GetKd(PID *pid);
+int PID_GetMode(PID *pid);
+int PID_GetDirection(PID *pid);
 
 /** Constructor (...) ******************************************************
  *  The parameters specified here are those for for which we can't set up  
  *  reliable defaults, so we need to have the user set them.               
  ***************************************************************************/
-void pid_controller(float *Input, float *Output, float *Setpoint, float Kp, float Ki, float Kd, int POn, int ControllerDirection)
+void PID_Init(PID *pid, float *Input, float *Output, float *Setpoint, float Kp, float Ki, float Kd, int POn, int ControllerDirection)
 {
-    myOutput = Output;
-    myInput = Input;
-    mySetpoint = Setpoint;
-    inAuto = false;
+    pid->myOutput = Output;
+    pid->myInput = Input;
+    pid->mySetpoint = Setpoint;
+    pid->inAuto = false;
 
-    set_output_limits(0, 65535);  // default output limit corresponds to
-                                     // the pico's pwm limits
+    PID_SetOutputLimits(pid, 0, 65535);  // default output limit corresponds to
+                                         // the pico's pwm limits
 
-    SampleTime = 100;                // default Controller Sample Time is 0.1 seconds
+    pid->SampleTime = 100;                // default Controller Sample Time is 0.1 seconds
 
-    set_controller_direction(ControllerDirection);
-    set_tunings(Kp, Ki, Kd, POn);
+    PID_SetControllerDirection(pid, ControllerDirection);
+    PID_SetTunings(pid, Kp, Ki, Kd, POn);
 
-    lastTime = to_ms_since_boot(get_absolute_time()) - SampleTime;
+    pid->lastTime = to_ms_since_boot(get_absolute_time()) - pid->SampleTime;
 }
+
+
+/** Constructor (...) ********************************************************
+ *  To allow backwards compatability for v1.1, or for people that just want  
+ *  to use Proportional on Error without explicitly saying so                
+ *****************************************************************************/
+void PID_Init_default(PID *pid, float *Input, float *Output, float *Setpoint, float Kp, float Ki, float Kd, int ControllerDirection)
+{
+    PID_Init(pid, Input, Output, Setpoint, Kp, Ki, Kd, P_ON_E, ControllerDirection);
+}
+
 
 /** Compute() ************************************************************************
  *  This, as they say, is where the magic happens.  this function should be called         
@@ -40,55 +117,55 @@ void pid_controller(float *Input, float *Output, float *Setpoint, float Kp, floa
  *  whether a new pid Output needs to be computed.  returns true when the output is            
  *  computed, false when nothing has been done.                                                      
  *************************************************************************************/
-bool compute()
+bool PID_Compute(PID *pid)
 {
-    if (!inAuto)
+    if (!pid->inAuto)
         return false;
     
     unsigned long now = to_ms_since_boot(get_absolute_time());
-    unsigned long timeChange = (now - lastTime);
+    unsigned long timeChange = (now - pid->lastTime);
 
-    if (timeChange >= SampleTime)
+    if (timeChange >= pid->SampleTime)
     {
         /* Compute all the working error variables */
-        float input = *myInput;
-        float error = *mySetpoint - input;
-        float dInput = (input - lastInput);
-        outputSum += (ki * error);
+        float input = *(pid->myInput);
+        float error = *(pid->mySetpoint) - input;
+        float dInput = (input - pid->lastInput);
+        pid->outputSum += (pid->ki * error);
 
         /* Add Proportional on Measurement, if P_ON_M is specified */
-        if (!pOnE)
-            outputSum -= kp * dInput;
+        if (!pid->pOnE)
+            pid->outputSum -= pid->kp * dInput;
 
-        if (outputSum > outMax)
-            outputSum = outMax;
+        if (pid->outputSum > pid->outMax)
+            pid->outputSum = pid->outMax;
 
-        else if (outputSum < outMin)
-            outputSum = outMin;
+        else if (pid->outputSum < pid->outMin)
+            pid->outputSum = pid->outMin;
 
         /* Add Proportional on Error, if P_ON_E is specified */
         float output;
         
-        if (pOnE)
-            output = kp * error;
+        if (pid->pOnE)
+            output = pid->kp * error;
         
         else
             output = 0;
 
         /* Compute Rest of PID Output */
-        output += outputSum - kd * dInput;
+        output += pid->outputSum - pid->kd * dInput;
 
-        if (output > outMax)
-            output = outMax;
+        if (output > pid->outMax)
+            output = pid->outMax;
 
-        else if (output < outMin)
-            output = outMin;
+        else if (output < pid->outMin)
+            output = pid->outMin;
 
-        *myOutput = output;
+        *(pid->myOutput) = output;
 
         /* Remember some variables for next time */
-        lastInput = input;
-        lastTime = now;
+        pid->lastInput = input;
+        pid->lastTime = now;
         return true;
     }
 
@@ -102,44 +179,54 @@ bool compute()
  *  it's called automatically from the constructor, but tunings can also
  *  be adjusted on the fly during normal operation
  *******************************************************************************/
-void set_tunings(float Kp, float Ki, float Kd, int POn)
+void PID_SetTunings(PID *pid, float Kp, float Ki, float Kd, int POn)
 {
     if (Kp < 0 || Ki < 0 || Kd < 0)
         return;
 
-    pOn = POn;
-    pOnE = POn == P_ON_E;
+    pid->pOn = POn;
+    pid->pOnE = POn == P_ON_E;
 
-    dispKp = Kp;
-    dispKi = Ki;
-    dispKd = Kd;
+    pid->dispKp = Kp;
+    pid->dispKi = Ki;
+    pid->dispKd = Kd;
 
-    float SampleTimeInSec = ((float)SampleTime) / 1000;
-    kp = Kp;
-    ki = Ki * SampleTimeInSec;
-    kd = Kd / SampleTimeInSec;
+    float SampleTimeInSec = ((float)pid->SampleTime) / 1000;
+    pid->kp = Kp;
+    pid->ki = Ki * SampleTimeInSec;
+    pid->kd = Kd / SampleTimeInSec;
 
-    if (controllerDirection == REVERSE)
+    if (pid->controllerDirection == REVERSE)
     {
-        kp = (0 - kp);
-        ki = (0 - ki);
-        kd = (0 - kd);
+        pid->kp = (0 - pid->kp);
+        pid->ki = (0 - pid->ki);
+        pid->kd = (0 - pid->kd);
     }
 }
+
+
+/** SetTunings(...) **********************************
+ *  Set Tunings using the last-remembered POn setting  
+ *****************************************************/
+void PID_SetTunings_basic(PID *pid, float Kp, float Ki, float Kd)
+{
+    PID_SetTunings(pid, Kp, Ki, Kd, pid->pOn);
+}
+
 
 /** SetSampleTime(...) *******************************************************
  *  sets the period, in Milliseconds, at which the calculation is performed
  *****************************************************************************/
-void set_sample_time(int NewSampleTime)
+void PID_SetSampleTime(PID *pid, int NewSampleTime)
 {
     if (NewSampleTime > 0)
     {
-        float ratio = (float)NewSampleTime / (float)SampleTime;
+        float ratio = (float)NewSampleTime / (float)pid->SampleTime;
 
-        ki *= ratio;
-        kd /= ratio;
+        pid->ki *= ratio;
+        pid->kd /= ratio;
 
-        SampleTime = (unsigned long)NewSampleTime;
+        pid->SampleTime = (unsigned long)NewSampleTime;
     }
 }
 
@@ -152,27 +239,27 @@ void set_sample_time(int NewSampleTime)
  *  want to clamp it from 0-125.  who knows.  at any rate, that can all be done
  *  here.
  **********************************************************************************/
-void set_output_limits(float Min, float Max)
+void PID_SetOutputLimits(PID *pid, float Min, float Max)
 {
     if (Min >= Max)
         return;
 
-    outMin = Min;
-    outMax = Max;
+    pid->outMin = Min;
+    pid->outMax = Max;
 
-    if (inAuto)
+    if (pid->inAuto)
     {
-        if (*myOutput > outMax)
-            *myOutput = outMax;
+        if (*(pid->myOutput) > pid->outMax)
+            *(pid->myOutput) = pid->outMax;
 
-        else if (*myOutput < outMin)
-            *myOutput = outMin;
+        else if (*(pid->myOutput) < pid->outMin)
+            *(pid->myOutput) = pid->outMin;
 
-        if (outputSum > outMax)
-            outputSum = outMax;
+        if (pid->outputSum > pid->outMax)
+            pid->outputSum = pid->outMax;
 
-        else if (outputSum < outMin)
-            outputSum = outMin;
+        else if (pid->outputSum < pid->outMin)
+            pid->outputSum = pid->outMin;
     }
 }
 
@@ -182,17 +269,17 @@ void set_output_limits(float Min, float Max)
  *  when the transition from manual to auto occurs, the controller is
  *  automatically initialized
  ********************************************************************************/
-void set_mode(int Mode)
+void PID_SetMode(PID *pid, int Mode)
 {
     bool newAuto = (Mode == AUTOMATIC);
     
-    if (newAuto && !inAuto)
+    if (newAuto && !pid->inAuto)
     { 
         /* we just went from manual to auto */
-        pid_initialize();
+        PID_Initialize(pid);
     }
 
-    inAuto = newAuto;
+    pid->inAuto = newAuto;
 }
 
 
@@ -200,16 +287,16 @@ void set_mode(int Mode)
  *  does all the things that need to happen to ensure a bumpless transfer
  *  from manual to automatic mode.
  ***************************************************************************/
-void pid_initialize()
+void PID_Initialize(PID *pid)
 {
-    outputSum = *myOutput;
-    lastInput = *myInput;
+    pid->outputSum = *(pid->myOutput);
+    pid->lastInput = *(pid->myInput);
 
-    if (outputSum > outMax)
-        outputSum = outMax;
+    if (pid->outputSum > pid->outMax)
+        pid->outputSum = pid->outMax;
 
-    else if (outputSum < outMin)
-        outputSum = outMin;
+    else if (pid->outputSum < pid->outMin)
+        pid->outputSum = pid->outMin;
 }
 
 
@@ -219,26 +306,27 @@ void pid_initialize()
  *  know which one, because otherwise we may increase the output when we should
  *  be decreasing.  This is called from the constructor.
  **********************************************************************************/
-void set_controller_direction(int Direction)
+void PID_SetControllerDirection(PID *pid, int Direction)
 {
-    if (inAuto && Direction != controllerDirection)
+    if (pid->inAuto && Direction != pid->controllerDirection)
     {
-        kp = (0 - kp);
-        ki = (0 - ki);
-        kd = (0 - kd);
+        pid->kp = (0 - pid->kp);
+        pid->ki = (0 - pid->ki);
+        pid->kd = (0 - pid->kd);
     }
 
-    controllerDirection = Direction;
+    pid->controllerDirection = Direction;
 }
 
 
-/** Status Funcions ***********************************************************
+/** Status Functions ***********************************************************
  *  Just because you set the Kp=-1 doesn't mean it actually happened.  these
  *  functions query the internal state of the PID.  they're here for display
- *  purposes.  this are the functions the PID Front-end uses for example
+ *  purposes.  these are the functions the PID Front-end uses for example
  ******************************************************************************/
-float get_kp()       { return dispKp; }
-float get_ki()       { return dispKi; }
-float get_kd()       { return dispKd; }
-int get_mode()       { return inAuto ? AUTOMATIC : MANUAL; }
-int get_direction()  { return controllerDirection; }
+float PID_GetKp(PID *pid)       { return pid->dispKp; }
+float PID_GetKi(PID *pid)       { return pid->dispKi; }
+float PID_GetKd(PID *pid)       { return pid->dispKd; }
+int PID_GetMode(PID *pid)       { return pid->inAuto ? AUTOMATIC : MANUAL; }
+int PID_GetDirection(PID *pid)  { return pid->controllerDirection; }
+
