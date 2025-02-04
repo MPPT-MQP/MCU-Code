e146a7d874dcaffdf4f325e939da8e878822f88b
Frank <fparsons143@gmail.com>
2025-02-03 22:34:12 -0500

    add date and time input

--- MCU_Code/user_interface.c
+++ MCU_Code/user_interface.c
@@ -2,6 +2,13 @@
 #include "buttons.h"
 #include "oled_screen.h"
 #include <string.h>
+#include <time.h>
+
+// Time Stuff
+ time_t current_time;
+ struct tm *time_info;
+ char date_buffer[80];
+ char time_buffer[80];
 
 /* Variables to keep track of which screen and setting is selected*/
 int screen_num = 0;
@@ -15,7 +22,7 @@ char *algorithms[] = {"CV", "BETA", "P&O", "P&O VAR", "INC-COND", "INC-COND VAR"
 char *current_mode = "BUCK";
 char *mode[] = {"MODE: BUCK", "MODE: CC", "MODE: BOTH"};
 char *current_sd_card = "SD CARD: ON";
-char *sd_card[] = {"SD CARD: ON", "SD CARD: OFF"};
+char *sd_card[] = {"SD CARD: OFF", "SD CARD: ON"};
 
 /*Toggle Variables to Keep Track of Settings*/
 // 0 = Tracking Off, 1 = Tracking On 
@@ -26,15 +33,23 @@ int algorithm_toggle = 0;
 int mode_toggle = 0;
 // 0 = OFF, 1 = ON
 int sd_card_toggle = 0;
-
+// 0 = Month, 1 = Day, 2 = Year, 3 = Hours, 4 = Minutes
+int date_time_toggle = 0;
+char *current_time_select;
 
 void welcome_screen(){
     char *screen1[] = {" ", "WPI MPPT MQP", "2024-2025", " "};
     int x_distances1[] = {1, 15, 30, 1};
     print_text(screen1, count_of(screen1), x_distances1);
+    
+    time(&current_time);
+    time_info = localtime(&current_time);
 }
 
 void run_main_screens() {
+
+    strftime(date_buffer, 80,"%x", time_info);
+    strftime(time_buffer, 80, "%I:%M%p", time_info);
 
     if(button1_state) {
         screen_num++;
@@ -132,25 +147,79 @@ void run_main_screens() {
         case 4:
             if(button2_state) {                
                 select_num++;
-                if(select_num > 2) {
+                if(select_num > 6) {
                    select_num = 0;
                 }
                 button2_state = !button2_state;
             }
             switch(select_num){
                 case 0:
+                    if(button3_state) {
+                            sd_card_toggle++;
+                        if(sd_card_toggle > 1) {
+                              sd_card_toggle = 0;
+                        }
+                         button3_state = !button3_state;
+                    }
+                    char *sd_card_selected[] = {"SD CARD: OFF<", "SD CARD: ON<"};
+                    current_sd_card = sd_card_selected[sd_card_toggle];
+                    char *screen4[] = {current_sd_card, "DATE & TIME", date_buffer, time_buffer};
+                    int x_distances4[] = {1, 1, 1, 1};
+                    print_text(screen4, count_of(screen4), x_distances4);
+                break;
 
+                default:
+                    if(button3_state) {
+                        switch(select_num-1){
+                            case 0:
+                                (time_info->tm_mon)++;
+                            break;
+                            case 1:
+                                (time_info->tm_mday)++;
+                            break;
+                            case 2:
+                                (time_info->tm_year)++;
+                            break;
+                            case 3:
+                                (time_info->tm_hour)++;
+                            break;
+                            case 4:
+                                (time_info->tm_min)++;
+                            break;
+                        }
+                        current_time = mktime(time_info);
+                        button3_state = !button3_state;
+                    }
+                    if(button4_state) {
+                        switch(select_num-1){
+                            case 0:
+                                (time_info->tm_mon)--;
+                            break;
+                            case 1:
+                                (time_info->tm_mday)--;
+                            break;
+                            case 2:
+                                (time_info->tm_year)--;
+                            break;
+                            case 3:
+                                (time_info->tm_hour)--;
+                            break;
+                            case 4:
+                                (time_info->tm_min)--;
+                            break;
+                        }
+                        current_time = mktime(time_info);
+                        button4_state = !button4_state;
+                    }
 
-
-
-
-
-
-
+                    char *date_time_selected[] = {"DATE & TIME MO", "DATE & TIME D", "DATE & TIME Y", "DATE & TIME H", "DATE & TIME MI"};
+                    current_time_select = date_time_selected[select_num-1];
+                    current_sd_card = sd_card[sd_card_toggle];
+                    char *screen41[] = {current_sd_card, current_time_select, date_buffer, time_buffer};
+                    int x_distances41[] = {1, 1, 1, 1};
+                    print_text(screen41, count_of(screen41), x_distances41);
+                break;
             }
-            char *screen4[] = {"SD CARD: ON", "Date & Time", "2025-02-02", "00:00:00"};
-            int x_distances4[] = {1, 1, 1, 1};
-            print_text(screen4, count_of(screen4), x_distances4);
         break;  
 
     }
