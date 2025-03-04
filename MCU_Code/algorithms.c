bbac46ab3a8e8b046e86f7e0fa6d63d83bb26ca7
Kyle Rabbitt <119811866+kyler26@users.noreply.github.com>
2025-03-04 09:48:35 -0500

    merge algo.c

--- MCU_Code/algorithms.c
+++ MCU_Code/algorithms.c
@@ -9,7 +9,7 @@ float P_O_step;
 static float I_C_step_val = 0.035;
 float I_C_step;
 
-float duty;
+float duty = 0.1;
 float voltage;
 float current;
 float power;
@@ -123,6 +123,18 @@ float pid_compute(PIDController *pid, float setpoint, float actual_value, float 
 
 <<<<<<< HEAD
 float Vref = 19.39;
+=======
+void duty_sweep(){
+
+    if (duty >= 0.95) {
+        duty = 0.1;
+    } else {
+        duty += 0.01;
+    }
+    printf("Voltage: %0.3f, Current: %0.3f, Duty: %0.3f\n", voltage, current, duty);
+}
+
+>>>>>>> fcaf8ba6d29e60e83959c3176aeaa64334df3552
 void constant_voltage() {
     float duty_raw;
     
